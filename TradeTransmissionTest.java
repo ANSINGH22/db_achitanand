186a613540c4e09c7c9d9e9b3b114e85c1346349
ANSINGH22 <41006877+ANSINGH22@users.noreply.github.com>
2021-11-26 17:49:01 +0530

    Add files via upload

--- TradeTransmissionTest.java
+++ TradeTransmissionTest.java
@@ -17,50 +17,18 @@ public class TradeTransmissionTest {
 
 	TradeFlow tflow = new TradeFlow();
 	Date currDate =Calendar.getInstance().getTime();
-	SimpleDateFormat dateform=new SimpleDateFormat("dd/MM/YYYY");
+	SimpleDateFormat dateform=new SimpleDateFormat("dd/mm/yyyy");
 	
-/*	@Test
-	public void testcheckVersion() throws Exception {
-		
-		Date maturityDate=dateform.parse("20/05/2021");
-        Trade t2=new Trade("T2",2,"CP-2","B1",maturityDate, currDate, 'N');
-        tflow.addTrade(t2);
-
-        //Changing Version as 3 and Changing Counter-Party ID to CP-3
-        Trade t3=new Trade("T2",3,"CP-3","B1",maturityDate, currDate, 'N');
-        tflow.addTrade(t3);
-        assertEquals("CP-4",tflow.allTrade.get("T2").getCounterPartId());
-        
-        maturityDate=dateform.parse("20/05/2021");
-        //Same Version as before and Changing Counter-Party ID to CP-2
-        Trade t4=new Trade("T1",1,"CP-2","B1",maturityDate, currDate, 'N');
-        tflow.addTrade(t4);
-        assertEquals("CP-2",tflow.allTrade.get("T1").getCounterPartId());
-    
-		
-	}
-	*/
-	/*
-	 * @Test public void testVersion() throws Exception { Date
-	 * maturityDate=dateform.parse("20/05/2021"); //Same Version as before and
-	 * Changing Counter-Party ID to CP-2 Trade t4=new
-	 * Trade("T1",1,"CP-2","B1",maturityDate, currDate, 'N'); tflow.addTrade(t4);
-	 * assertEquals("CP-2",tflow.allTrade.get("T1").getCounterPartId()); }
-	 */
-
 	@Test
     public void testVersionLow() throws Exception
     {
-        Date maturityDate=dateform.parse("20/12/2021");
-
-        Trade t5=new Trade("T3",1,"CP-3","B1",maturityDate, currDate, 'N');
-        tflow.addTrade(t5);
-        //tflow.allTrade.put("T3", t5);
-        int sizeofList=tflow.allTrade.size();
-        //Now Adding Another List
-        Trade t6=new Trade("T3",0,"CP-2","B1",maturityDate, currDate, 'N');
-
-        assertThrows(Exception.class,()->tflow.addTrade(t6),"0 is less than 1");
+		SimpleDateFormat dateform=new SimpleDateFormat("dd/mm/yyyy");
+		Date maturityDate=dateform.parse("20/12/2021");
+        Date todaysDate=Calendar.getInstance ().getTime ();
+        //Same Version as before and Changing Counter-Party ID to CP-2
+        Trade t4=new Trade("T1",1,"CP-2","B1",maturityDate, todaysDate, 'N');
+        tflow.addTrade(t4);
+        assertEquals("CP-2",tflow.allTrade.get("T1").getCounterPartId());
 
     }
 
@@ -68,20 +36,16 @@ public class TradeTransmissionTest {
     @Test
     public void testMaturityGreater() throws Exception
     {
-        Date maturityDate=dateform.parse("30/12/2021");
 
-        Trade t7=new Trade("T4",5,"CP-4","B3",maturityDate, currDate, 'N');
-        tflow.addTrade(t7);
-        System.out.println(" "+t7);
-        
-        System.out.println("output: = "+tflow.allTrade.get("T4"));
-        
-        System.out.println("outputmap: = "+tflow.allTrade);
+    	Date todaysDate=Calendar.getInstance ().getTime ();
+        Date maturityDate=dateform.parse("20/05/2022");
+		Trade t2=new Trade("T2",2,"CP-2","B1",maturityDate, todaysDate, 'N');
+		tflow.addTrade(t2);
 
-        assertEquals(t7,tflow.allTrade.get("T4"));
+        assertEquals(t2,tflow.allTrade.get("T4"));
 
     }
-
+   
  
     @Test
     public void testMaurityLower() throws Exception
