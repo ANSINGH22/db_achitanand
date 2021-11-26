5905d50ea33de993b1ab8e3012a33c62a1715dfb
ANSINGH22 <41006877+ANSINGH22@users.noreply.github.com>
2021-11-26 17:14:13 +0530

    Add files via upload

--- TradeTransmissionTest.java
+++ TradeTransmissionTest.java
@@ -40,39 +40,43 @@ public class TradeTransmissionTest {
 		
 	}
 	*/
-	@Test
-    void testVersion() throws Exception
-    {
-        Date maturityDate=dateform.parse("20/05/2021");
-        //Same Version as before and Changing Counter-Party ID to CP-2
-        Trade t4=new Trade("T1",1,"CP-2","B1",maturityDate, currDate, 'N');
-        tflow.addTrade(t4);
-        assertEquals("CP-2",tflow.allTrade.get("T1").getCounterPartId());
-    }
+	/*
+	 * @Test public void testVersion() throws Exception { Date
+	 * maturityDate=dateform.parse("20/05/2021"); //Same Version as before and
+	 * Changing Counter-Party ID to CP-2 Trade t4=new
+	 * Trade("T1",1,"CP-2","B1",maturityDate, currDate, 'N'); tflow.addTrade(t4);
+	 * assertEquals("CP-2",tflow.allTrade.get("T1").getCounterPartId()); }
+	 */
 
 	@Test
-    void testVersionLow() throws Exception
+    public void testVersionLow() throws Exception
     {
         Date maturityDate=dateform.parse("20/12/2021");
 
-        Trade t5=new Trade("T3",5,"CP-3","B1",maturityDate, currDate, 'N');
+        Trade t5=new Trade("T3",1,"CP-3","B1",maturityDate, currDate, 'N');
         tflow.addTrade(t5);
+        //tflow.allTrade.put("T3", t5);
         int sizeofList=tflow.allTrade.size();
         //Now Adding Another List
-        Trade t6=new Trade("T3",1,"CP-2","B1",maturityDate, currDate, 'N');
+        Trade t6=new Trade("T3",0,"CP-2","B1",maturityDate, currDate, 'N');
 
-        assertThrows(Exception.class,()->tflow.addTrade(t6),"1 is less than 5");
+        assertThrows(Exception.class,()->tflow.addTrade(t6),"0 is less than 1");
 
     }
 
 	
     @Test
-    void testMaturityGreater() throws Exception
+    public void testMaturityGreater() throws Exception
     {
-        Date maturityDate=dateform.parse("20/05/2021");
+        Date maturityDate=dateform.parse("30/12/2021");
 
         Trade t7=new Trade("T4",5,"CP-4","B3",maturityDate, currDate, 'N');
         tflow.addTrade(t7);
+        System.out.println(" "+t7);
+        
+        System.out.println("output: = "+tflow.allTrade.get("T4"));
+        
+        System.out.println("outputmap: = "+tflow.allTrade);
 
         assertEquals(t7,tflow.allTrade.get("T4"));
 
@@ -80,7 +84,7 @@ public class TradeTransmissionTest {
 
  
     @Test
-    void testMaurityLower() throws Exception
+    public void testMaurityLower() throws Exception
     {
         Date maturityDate=dateform.parse("20/05/2020");
         Trade t8=new Trade("T5",1,"CP-4","B3",maturityDate, currDate, 'N');
